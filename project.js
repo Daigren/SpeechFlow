c3a3871bc0e822cdadb7fe40922b11e85158aa0a
daigren <rykovgrisa53@gmail.com>
2025-11-18 23:05:00 +0300

    improve(project.js): объеденены две функции в одну, начата работа над скролом по кнопкам

--- project.js
+++ project.js
@@ -1,17 +1,14 @@
-
 const createButton = document.getElementById('create');
 const sidebar = document.querySelector('aside');
 const startButton = document.querySelector('.start');
 const timer = document.getElementById('timerButton');
 const main = document.querySelector('main');
 const line = document.getElementById('line');
-const down = document.getElementById('down');
-const up = document.getElementById('up');
 const message = document.getElementById('message1');
 
-let noteCount = 1;
-let messageCount = 1;
+let noteCount = 11;
 let timerCount = 1;
+let newNoteScroll
 
 // ======================скролл
 
@@ -22,56 +19,47 @@ function scrollUp() {
     message.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
 
-// ======================Поле для ввода
-
 function createNewMessage() {
-    messageCount++;
     timerCount++;
+    noteCount++
 
     const newMessage = document.createElement('textarea');
     const newTimer = document.createElement('button');
+    const newNote = document.createElement('button');
 
+// ======================Поле для ввода
     newMessage.className = "bg-[#383838] text-white border border-[#262626] rounded-[20px] p-4 resize-none mx-auto block text-1";
-    
-    newMessage.id = "message" + messageCount; 
+    newMessage.id = timerCount; 
     newMessage.rows = 20;
     newMessage.cols = 100;
     newMessage.placeholder = "";
     newMessage.maxLength = 500;
 
+// ======================Кнопка таймера 
     newTimer.className = 'note bg-[#383838] h-[45px] w-100 mt-3 flex items-center px-4 group hover:bg-gray-500 transition-colors rounded-[10px] p-4 resize-none mx-auto block mb-3';
     newTimer.innerHTML = `<span class="text-[#5ffeb9] text-[24px]">create a timer ${timerCount}</span>
-    <img src="./files/Timer.svg" class="ml-3 w-[18px]">
+    <img src="./files/Timer.svg" class="ml-3 w-[18px]" id="createTimer${timerCount}">
     `;
+
+// ======================Кнопка Note
+    newNote.className = 'bg-[#262626] rounded-[10px] h-[45px] w-full mt-8 flex items-center justify-between px-4 group hover:bg-gray-600 transition-colors';
+    newNote.innerHTML = `<span class="text-white text-[24px]" onclick="scrollNote(id)">Note ${timerCount}</span>`;
+    newNote.id = noteCount;
 
     main.insertBefore(newMessage, line);
     main.insertBefore(newTimer, line);
-
+    sidebar.insertBefore(newNote, createButton);
 
     newTimer.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+    newNoteScroll = newMessage.id
 }
 
-// ======================Кнопка "Создать"
+// ======================Скролл по нажатию на Note
 
-function createNewNote() {
-    noteCount++;
-    
-    const newNote = document.createElement('button');
-    newNote.className = 'note bg-[#262626] rounded-[10px] h-[45px] w-full mt-8 flex items-center justify-between px-4 group hover:bg-gray-600 transition-colors';
-    newNote.innerHTML = `<span class="text-white text-[24px]">Note ${noteCount}</span>`;
+function scrollNote(numNote) {
+    let numMessage
+    numMessage = numNote - 10;
 
-    
-    sidebar.insertBefore(newNote, createButton);
-    
-    newNote.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    numMessage.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
-
-// Добавляем обработчик на кнопку "Create new +"
-createButton.addEventListener('click', createNewNote);
-createButton.addEventListener('click', createNewMessage);
-down.addEventListener('click', scrollDown);
-up.addEventListener('click', scrollUp);
-
-
-
-
